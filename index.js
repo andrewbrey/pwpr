aecd027c5c1e083352b37e29736d9004ee11ad86
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2021-08-03 17:27:16 -0700

    docs(*): update help text and readme with new options and examples

--- index.js
+++ index.js
@@ -5,14 +5,20 @@ const { chromium } = require('playwright');
 
 const argv = minimist(process.argv.slice(2));
 
+const def = {
+  networkTimeout: 30000,
+  jsTimeout: 5000,
+  sleep: 1000,
+};
+
 const options = {
   help: argv.h || argv.help || argv._[0] === 'help',
   url: (argv.url || '').trim(),
-  networkTimeout: Number(argv.network) || 30000,
-  jsTimeout: Number(argv.js) || 5000,
-  sleep: Number(argv.sleep) || 1000,
-  domSelector: (argv.selector || '').trim(),
   outputPath: (argv.output || '').trim(),
+  networkTimeout: Number(argv.network) || def.networkTimeout,
+  jsTimeout: Number(argv.js) || def.jsTimeout,
+  domSelector: (argv.selector || '').trim(),
+  sleep: Number(argv.sleep) || def.sleep,
   showBrowser: Boolean(argv.show),
   debug: Boolean(argv.debug),
 };
@@ -65,7 +71,10 @@ ${chalk.yellow('Missing required option --url. Run again with the --help option 
 function help() {
   const bin = 'pwpr';
 
-  console.log(`
+  console.log(`-----------------------------------
+${chalk.green.bold(bin.toLocaleUpperCase())} - the Playwright Prerender CLI
+-----------------------------------
+
 A small utility wrapper around ${chalk.bold('playwright')} [${chalk.dim.underline(
     'https://playwright.dev'
   )}] which makes it simple
@@ -78,19 +87,45 @@ ${chalk.bold('OPTIONS')}
   -h, --help    ${chalk.dim('show CLI help')}
   --url         [${chalk.bold(['required'])}] ${chalk.dim('the url to visit and prerender (must have an http or https')}
                 ${chalk.dim('protocol; anything else will have an https protocol prepended to it)')}
-  --output      [${chalk.italic('optional')}] ${chalk.dim(
-    'a valid file path into which the page content should be saved'
-  )}
+  --output      ${chalk.dim('a valid file path into which the page content should be saved')}
                 ${chalk.dim('(if omitted, page content will print to stdout)')}
+  --network     ${chalk.dim(`the timeout in milliseconds to wait for the page to load (default ${def.networkTimeout})`)}
+  --js          ${chalk.dim(`the timeout in milliseconds to wait for JS execution (default ${def.jsTimeout})`)}
+                ${chalk.dim('(only used if option --selector is provided)')}
+  --selector    ${chalk.dim(`the css selector to wait for before capturing the page (default ${def.jsTimeout})`)}
+  --sleep       ${chalk.dim(`the timeout in milliseconds to wait before capturing the page (default ${def.sleep})`)}
+                ${chalk.dim('(only used if option --selector is *not* provided)')}
+  --show        ${chalk.dim('if provided and not empty, will cause the browser to be shown (default false)')}
+  --debug       ${chalk.dim('enable debug output (default false)')}
+
 
 ${chalk.bold('EXAMPLES')}
-    npx ${bin} --url=http://example.com  ${chalk.green('// prerenders http://example.com and prints to stdout')}
-    npx ${bin} --url=example.com  ${chalk.green('// prerenders https://example.com and prints to stdout')}
-    npx ${bin} --url=example.com --output=example.html  ${chalk.green(
-    '// prerenders https://example.com and saves to ./example.html'
-  )}
-    npx ${bin} --url=file://somefile.txt --output=example.html  ${chalk.green(
-    '// tries to prerender https://file://somefile.txt and blows up'
-  )}
+    npx ${bin} --url=http://example.com
+    ${chalk.green('# prerenders http://example.com and prints to stdout')}
+
+    npx ${bin} --url=example.com
+    ${chalk.green('# prerenders https://example.com and prints to stdout')}
+
+    npx ${bin} --url=example.com --network=60000
+    ${chalk.green('# prerenders https://example.com after allowing *up to* 60 seconds for the')}
+    ${chalk.green('# document to fire the "load" event, and prints to stdout')}
+
+    npx ${bin} --url=example.com --output=example.html 
+    ${chalk.green('# prerenders https://example.com and saves to ./example.html')}
+
+    npx ${bin} --url=example.com --sleep=5000
+    ${chalk.green('# prerenders https://example.com after waiting for 5 seconds')}
+
+    npx ${bin} --url=example.com --js=10000 --selector='.js-loaded-class-name'
+    ${chalk.green('# prerenders https://example.com after waiting for *up to* 10 seconds for the specified')}
+    ${chalk.green('# selector to be matched in the page with "docment.querySelector()" and prints to stdout')}
+
+    npx ${bin} --url=example.com --js=10000 --selector='.js-loaded-class-name' --sleep=10000
+    ${chalk.green('# prerenders https://example.com after waiting for *up to* 10 seconds for the specified')}
+    ${chalk.green('# selector to be matched in the page with "docment.querySelector()" and prints to stdout')}
+    ${chalk.green('# **note that the --sleep option is ignored because the --selector option was provided')}
+
+    npx ${bin} --url=file://somefile.txt --output=example.html
+    ${chalk.green('# tries to prerender https://file://somefile.txt and blows up')}
 `);
 }
